42a8b07cae0ded257108e67ba20ba2693ec2a11c
khannuuuuur <71348973+khannuuuuur@users.noreply.github.com>
2021-04-23 20:32:26 -0700

    Add files via upload

--- Steganography.py
+++ Steganography.py
@@ -15,18 +15,18 @@ def stringToBinary(string):
     output = ""
     for c in string:
         num = ''.join(format(ord(c), 'b'))
-        
+
         num = '0'*(7-len(num)) + num
         output += num
     return output
 
-def binaryToString(binary): 
+def binaryToString(binary):
     str_data =''
-    for i in range(0, len(binary), 7): 
-          
-        temp_data = int(binary[i:i + 7]) 
+    for i in range(0, len(binary), 7):
 
-        decimal_data = binaryToDecimal(temp_data) 
+        temp_data = int(binary[i:i + 7])
+
+        decimal_data = binaryToDecimal(temp_data)
 
         str_data = str_data + chr(decimal_data)
     return str_data
@@ -40,7 +40,7 @@ def binaryToDecimal(binary):
         i += 1
     return decimal
 
-def decimalToBinary(n):  
+def decimalToBinary(n):
     return bin(n).replace("0b", "")
 
 
@@ -56,8 +56,8 @@ def encodeImage(image, message, name, removeBlanks=False):
                 if image[row][col][1]%2 != int(binary[index]):
                     image[row][col][1] += int(binary[index]) - image[row][col][1]%2
 
-  
-                index += 1         
+
+                index += 1
             elif removeBlanks: #sets all the rest to zero.
                 if image[row][col][1]%2 != 0:
                     image[row][col][1] -= 1
@@ -81,28 +81,29 @@ def decodeImage(image, length):
                 return binaryToString(binary)
     return binaryToString(binary)
 
+def main():
+    while True:
+        option = int(input("Steganographic Processing:\n\t(1) Encode image\n\t(2) Decode image\n\t(3) Exit\nEnter an option: "))
+        if option == 1:
+            inFile = input("\nEnter an input file name: ")
+            outFile = input("Enter an output file name: ")
+            textFile = open(input("Enter a text file name to read from: "), 'r')
+            removeGarbage = True if input("Remove garbage characters? (y/n) ") == 'y' else False
+            encodeImage(openImage(inFile), textFile.read(), outFile, removeBlanks = removeGarbage)
+            print("\nDone.\n")
+        elif option == 2:
+            inFile = input("\nEnter an input file name: ")
+            length = int(input("Enter in how many characters to print out (-1 for all characters): "))
+            print()
+            print("="*60)
+            print()
+            print(decodeImage(openImage(inFile), length))
+            print()
+            print("="*60)
+            print()
+        else:
+            print("\n"*2)
+            break
 
-while True:
-    option = int(input("Steganographic Processing:\n\t(1) Encode image\n\t(2) Decode image\n\t(3) Exit\nEnter an option: "))
-    if option == 1:
-        inFile = input("\nEnter an input file name: ")
-        outFile = input("Enter an output file name: ")
-        textFile = open(input("Enter a text file name to read from: "), 'r')
-        removeGarbage = True if input("Remove garbage characters? (y/n) ") == 'y' else False
-        encodeImage(openImage(inFile), textFile.read(), outFile, removeBlanks = removeGarbage)
-        print("\nDone.\n")
-    elif option == 2:
-        inFile = input("\nEnter an input file name: ")
-        length = int(input("Enter in how many characters to print out (-1 for all characters): "))
-        print()
-        print("="*60)
-        print()
-        print(decodeImage(openImage(inFile), length))
-        print()
-        print("="*60)
-        print()
-    else:
-        break
-
-
-
+if __name__ == "__main__":
+    main()
